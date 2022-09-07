b19c9722e3279f9c00ab4971da97b2f006885d7e
msshp <eu.nevcskaya@yandex.ru>
2022-09-07 15:44:55 +0300

    Задание 2.5

---
+++ lesson-2/script5.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let a, b, c;
+
+function addition(a, b) {
+    return c = a + b;
+}
+
+function subtraction(a, b) {
+    return c = a - b;
+}
+
+function multiplication(a, b) {
+    return c = a * b;
+}
+
+function division(a, b) {
+    return c = a / b;
+}
+
+multiplication(3, 5);
+console.log(c);
